16d8050a012b5fe38f7937e37cd34c5fecbec201
mohamed-abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2025-07-23 14:53:04 +0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const connectToDatabase = require("./config/db");
 const authRoutes = require("./routes/auth.routes");
 const audioRoutes = require("./routes/audio.routes");
-const { default: mongoose } = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/audios", audioRoutes);
+
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -23,7 +23,9 @@ app.use((err, req, res, next) => {
 connectToDatabase()
   .then(() => {
     console.log("Database connected successfully 🎉");
-    app.listen(PORT);
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
   })
   .catch((err) => {
     console.error("Database connection failed:", err);
