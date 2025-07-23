d7f363e6b1545191264170a9fd8d8a8636b8017d
mohamed-abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2025-07-23 14:19:47 +0300

    commit

--- index.js
+++ index.js
@@ -8,30 +8,25 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/api/auth", authRoutes);
-// app.use("/api/audios", audioRoutes);
-app.get("/", (req, res) => {
-  res.send("hi");
-});
-// // Global error handler
-// app.use((err, req, res, next) => {
-//   res.status(err.status || 500).json({
-//     success: false,
-//     error: err.message || "Internal Server Error",
-//   });
-// });
+app.use("/api/auth", authRoutes);
+app.use("/api/audios", audioRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    error: err.message || "Internal Server Error",
+  });
 });
 
-// connectToDatabase()
-//   .then(() => {
-//     console.log("Database connected successfully 🎉");
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("Database connection failed:", err);
-//   });
+connectToDatabase()
+  .then(() => {
+    console.log("Database connected successfully 🎉");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:", err);
+  });
